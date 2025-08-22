dc9aa4711e607c974ff4b2da1e08101d91b98814
1pharaxh <pharaohsupreme03@gmail.com>
2025-08-21 22:31:47 -0400

    feat: implement ipc based themeing

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,15 @@
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string
 declare const MAIN_WINDOW_VITE_NAME: string
 
+// Preload types
+interface ThemeModeContext {
+  toggle: () => Promise<boolean>
+  dark: () => Promise<void>
+  light: () => Promise<void>
+  system: () => Promise<boolean>
+  current: () => Promise<'dark' | 'light' | 'system'>
+}
+
 interface ElectronWindow {
   minimize: () => Promise<void>
   maximize: () => Promise<void>
@@ -12,5 +21,6 @@ interface ElectronWindow {
 }
 
 declare interface Window {
+  themeMode: ThemeModeContext
   electronWindow: ElectronWindow
 }
